2d3ed300198f245fa0770d99c61ae14351a1d89a
Shreyas-hub-coder <shreyas.garg2005@gmail.com>
2024-08-05 00:09:57 +0530

    added AI

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,6 +1,6 @@
 import { UserButton } from '@clerk/nextjs'
 import React from 'react'
-import AddNewInterview from './_components/AddNewInterview'
+import AddNewInterview from '../../components/AddNewInterview'
 
 function Dashboard() {
   return (
---
+++ components/AddNewInterview.jsx
@@ -0,0 +1,102 @@
+"use client"
+import React, { useState } from 'react'
+import { Button } from "/components/ui/button";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "/components/ui/dialog"
+
+import { Input } from "/components/ui/input"
+import { Textarea } from "/components/ui/textarea"
+import { chatSession } from '/utils/GeminiAIModal';
+import { LoaderCircle } from 'lucide-react';
+import { db } from '/utils/db';
+import { MockInterview } from '/utils/schema';
+
+function AddNewInterview() {
+  const [openDialog, setOpenDialog] = useState(false)
+ 
+  const [jobPosition, setJobPosition] = useState()
+  const [jobDesc, setJobDesc] = useState()
+  const [jobExperience, setJobExperience] = useState()
+
+  const [loading , setLoading] = useState(false)
+  const [jsonResponse, setJsonResponse] = useState([])
+
+  const onSubmit = async(e) => {
+    setLoading(true)
+    e.preventDefault()
+    console.log(jobPosition, jobDesc, jobExperience)
+
+    const InputPrompt="given the job position: "+jobPosition+" and job description: "+jobDesc+" and years of experience: "+jobExperience+" years depending on these parameters generate 5 interview questions (technical questions preferably) for the candidate along with the an ideal answer in JSON format give us the questions and answers field in JSON format just give questions and answers and no other notes or comments";
+
+    const result = await chatSession.sendMessage(InputPrompt);
+    const MockJsonResp=(result.response.text()).replace('```json','').replace('```','')
+    console.log(JSON.parse(MockJsonResp));
+
+    setJsonResponse(MockJsonResp);
+
+    const resp = await db.insert(MockInterview)
+
+    setLoading(false);
+  }
+
+
+  return (
+    <div>
+      <div className='p-10 border rounded-lg bg-secondary hover:scale-105 hover:shadow-md cursor-pointer transition-all' onClick={() => setOpenDialog(true)}>
+        <h2 className=' text-large text-center'>
+          + Add New Interview
+        </h2>
+      </div>
+      
+      <Dialog open={openDialog} onOpenChange={setOpenDialog}>
+        <DialogContent className='max-w-2xl'>
+          <DialogHeader>
+            <DialogTitle className='text-2xl'>Tell us more about the job you want to interview for</DialogTitle>
+        
+            <DialogDescription>
+            <form onSubmit={onSubmit}>
+            <div>
+            
+              <h2>Add details about your job role, job description and years of experience</h2>
+              <div className='mt-7 my-3'>
+                <label>Job Role/Position</label>
+                <Input placeholder ="Ex: Full Stack developer" required onChange={(event)=>setJobPosition(event.target.value)}/>
+              </div>
+              <div className='my-3'>
+                <label>Job Description/Tech Stack</label>
+                <Textarea placeholder ="Ex: React, NodeJs, MySql etc." required onChange={(event)=>setJobDesc(event.target.value)}/>
+              </div>
+              <div className='my-3'>
+                <label>Your years of experience</label>
+                <Input placeholder ="Ex: 2, if you have worked in the field for 2 years" type="number" max="50" required onChange={(event)=>setJobExperience(event.target.value)}/>
+              </div>
+
+            </div>
+              <div className='flex gap-5 justify-end'>
+                <Button type="button" variant="ghost" onClick={() => setOpenDialog(false)}>Cancel</Button>
+                <Button type="submit" disabled={loading}>
+                  {loading ? <>
+                  <LoaderCircle className='animate-spin'/>Generating your personal AI Interview
+                  </> : 'Start Interview'
+                  }
+                  
+                </Button>
+              </div>
+              </form> 
+            </DialogDescription>
+            
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+    </div>
+  )
+}
+
+export default AddNewInterview
